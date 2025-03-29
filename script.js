d613e60389b806250b5802a9d984e4d22b25c568
SlytherinKid <mr.shubhamkr2000@gmail.com>
2025-03-29 23:59:19 +0530

    updated animations.

--- script.js
+++ script.js
@@ -92,7 +92,103 @@ textElement.addEventListener("click", () => {
 changeBackground();
 }
 
+function text3Animation(){
+    const images = [
+    {
+        url: "https://freakins.com/cdn/shop/files/Freakins15NOV1532.jpg?v=1732290612&width=800",
+        link: "https://freakins.com/products/denim-bustier-top?variant=42759787708577"
+    },
+    {
+        url: "https://freakins.com/cdn/shop/files/SAR03057.jpg?v=1733825567&width=800",
+        link: "https://freakins.com/products/brawl-beige-mens-cargo-korean-pants?_pos=11&_fid=3dbf70829&_ss=c?variant=51712105152876"
+    },
+    {
+        url: "https://freakins.com/cdn/shop/files/Freakins15NOV1447.jpg?v=1732290151&width=800",
+        link: "https://freakins.com/products/regal-brown-mens-stylised-korean-pants?_pos=27&_fid=3dbf70829&_ss=c&variant=51689140355436?variant=51689140355436"
+    },
+    {
+        url: "https://freakins.com/cdn/shop/files/1stMarchFreakinsECOM2997_9e68a26c-25e1-4a73-8afd-4a58771a2ba5.jpg?v=1742364459&width=800",
+        link: "https://freakins.com/products/ecru-distressed-mens-straight-jeans-optical?_pos=28&_fid=ef9fac605&_ss=c&variant=51866920911212?variant=51866920911212"
+    },
+    {
+        url: "https://freakins.com/cdn/shop/files/16july_19443-Edit-2_3e02e4d9-bd33-43d1-9654-cf7133e9daea.jpg?v=1721356759&width=800",
+        link: "https://freakins.com/products/dust-storm-straight-fit-jeans?variant=44491298570401"
+    }
+];
+
+let currentIndex = 0;
+const textElement = document.getElementById("text3");
+
+// Function to change background image
+function changeBackground() {
+    textElement.style.backgroundImage = `url(${images[currentIndex].url})`;
+}
+
+// Change image every 5 seconds
+setInterval(() => {
+    currentIndex = (currentIndex + 1) % images.length;
+    changeBackground();
+}, 4000);
+
+// Click event to redirect
+textElement.addEventListener("click", () => {
+    window.location.href = images[currentIndex].link;
+});
+
+// Initial setup
+changeBackground();
+}
+
+function text4Animation(){
+    const images = [
+    {
+        url: "https://freakins.com/cdn/shop/files/22April_3809-Edit.jpg?v=1713990446&width=800",
+        link: "https://freakins.com/products/denim-bustier-top?variant=42759787708577"
+    },
+    {
+        url: "https://freakins.com/cdn/shop/files/21MAY01871-Edit.jpg?v=1716528575&width=800",
+        link: "https://freakins.com/products/tropical-mesh-shirt?_pos=39&_fid=3fcd3bc38&_ss=c&variant=44331692851361?variant=44331692851361"
+    },
+    {
+        url: "https://freakins.com/cdn/shop/files/21MAY0371-Edit.jpg?v=1716528582&width=800",
+        link: "https://freakins.com/products/basic-camo-mesh-shirt?_pos=60&_fid=3fcd3bc38&_ss=c&variant=44331896373409?variant=44331896373409"
+    },
+    {
+        url: "https://freakins.com/cdn/shop/files/1stMarchFreakinsECOM3603.jpg?v=1741256860&width=800",
+        link: "https://freakins.com/products/white-ripped-mens-jeans-optical?variant=51866924351852"
+    },
+    {
+        url: "https://freakins.com/cdn/shop/files/Freakins15NOV0962.jpg?v=1732290913&width=800",
+        link: "https://freakins.com/products/icy-blue-mens-baggy-fit-jeans?_pos=8&_fid=c546eff98&_ss=c?variant=51687311212908"
+    }
+];
+
+let currentIndex = 0;
+const textElement = document.getElementById("text4");
+
+// Function to change background image
+function changeBackground() {
+    textElement.style.backgroundImage = `url(${images[currentIndex].url})`;
+}
+
+// Change image every 5 seconds
+setInterval(() => {
+    currentIndex = (currentIndex + 1) % images.length;
+    changeBackground();
+}, 4750);
+
+// Click event to redirect
+textElement.addEventListener("click", () => {
+    window.location.href = images[currentIndex].link;
+});
+
+// Initial setup
+changeBackground();
+}
+
 text1Animation()
 text2Animation()
+text3Animation()
+text4Animation()
 
 
